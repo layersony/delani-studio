7a3b55642e311dda61d6230b930f7aed566a7cc3
layersony <sammaingi5@gmail.com>
2021-04-16 01:19:48 +0300

    hover effect workin but not inside image

--- js/script.js
+++ js/script.js
@@ -1,5 +1,49 @@
 $(document).ready(function(){
-  $(".image").hover(function(){
-    alert("just did it");
-  })
-})
+  $(".image").mouseover(function(){
+    $(".txt-hide").show()
+  }).mouseout(function(){
+    $(".txt-hide").hide();
+  });
+
+  $(".image1").mouseover(function(){
+    $(".txt-hide1").show()
+  }).mouseout(function(){
+    $(".txt-hide1").hide();
+  });
+
+  $(".image2").mouseover(function(){
+    $(".txt-hide2").show()
+  }).mouseout(function(){
+    $(".txt-hide2").hide();
+  });
+
+  $(".image3").mouseover(function(){
+    $(".txt-hide3").show()
+  }).mouseout(function(){
+    $(".txt-hide3").hide();
+  });
+
+  $(".image4").mouseover(function(){
+    $(".txt-hide4").show()
+  }).mouseout(function(){
+    $(".txt-hide4").hide();
+  });
+
+  $(".image5").mouseover(function(){
+    $(".txt-hide5").show()
+  }).mouseout(function(){
+    $(".txt-hide5").hide();
+  });
+
+  $(".image6").mouseover(function(){
+    $(".txt-hide6").show()
+  }).mouseout(function(){
+    $(".txt-hide6").hide();
+  });
+
+  $(".image7").mouseover(function(){
+    $(".txt-hide7").show()
+  }).mouseout(function(){
+    $(".txt-hide7").hide();
+  });
+});
